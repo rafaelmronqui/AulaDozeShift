336fbb9d83a74696aaf6f67be77f8b57d98cd9e2
rafaelmronqui <rafaelmronqui@gmail.com>
2022-06-24 17:00:27 -0300

    terceiro

--- AulaDozeShift/src/teste/Programa.java
+++ AulaDozeShift/src/teste/Programa.java
@@ -3,6 +3,7 @@ package teste;
 public class Programa {
 
 	public static void main(String[] args) {
+	//teste
 		System.out.println("Oi");
 		System.out.println("Oi dois.");
 		System.out.println("Teste....");
